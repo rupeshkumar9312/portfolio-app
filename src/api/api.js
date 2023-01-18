86a93c2f9334e958ec5a085fed2a8e375b412d85
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-18 16:25:15 +0530

    home-api integrated

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const api = {
+    home: "http://www.localhost:8080/api/home",
+    about:"http://www.localhost:8080/api/about",
+    skills:"http://www.localhost:8080/api/skills",
+    education:"http://www.localhost:8080/api/education",
+    experience:"http://www.localhost:8080/api/experience",
+    contact:"http://www.localhost:8080/api/contact"
+
+}
+
+export default api;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,21 +4,34 @@ import Container from '../../container/Container'
 import ImageCard from '../../container/ImageCard'
 import Button from '../UI/Button'
 import './home.scss'
-import {easeIn, motion} from 'framer-motion'
+import { easeIn, motion } from 'framer-motion'
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import api from '../../api/api';
 function Home() {
+  const [home, setHome] = useState('')
+
+  useEffect(() => {
+    axios.get(api.home)
+      .then(res => {
+        setHome(res.data)
+      })
+  })
+
   return (
     <div>
       <Container>
         <motion.div className='child-container'
-        
-      >
+
+        >
           <p>
-            <h1>Hello 👋 I'm</h1>
-            <h1 className='text-name'>Rupesh Kumar</h1>
-            <p className='designation'>Software Developer</p>
+            <h1>{home.greetings} 👋 I'm</h1>
+            <h1 className='text-name'>{home.name}</h1>
+            <p className='designation'>{home.designation}</p>
             <Button>Get Resume <FaDownload></FaDownload></Button>
           </p>
-          
+
         </motion.div>
         <ImageCard></ImageCard>
 
